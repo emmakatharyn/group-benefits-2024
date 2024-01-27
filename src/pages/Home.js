d4ec6aa1029b44d235487f6702d91721e76103ee
Emma Reed <ereed@easitpa.com>
2024-01-26 21:37:41 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+// import { Carousel } from "../Carousel/Carousel";
+// import "../Carousel/carousel.css";
+
+function Home() {
+  //   return <Carousel />;
+  return <div>HOME</div>;
+}
+
+export default Home;
