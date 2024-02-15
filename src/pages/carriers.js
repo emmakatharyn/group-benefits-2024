b974314217790c1b3f2cba1afeeb018a5bf19c58
Emma Reed <ereed@easitpa.com>
2024-02-15 08:41:51 -0700

    start adding content to carriers.js

---
+++ src/pages/carriers.js
@@ -0,0 +1,554 @@
+const carriers = [
+  //EBB
+  {
+    carrierName: "Employee Benefits Bureau",
+    imgURL: "/images/2019open.jpg",
+    webAddress: "https://www.mybenefitsnm.com/",
+    modalID: "employeeBenefitsBureau",
+    content: {
+      openEnrollmentPY24: [
+        {
+          linkName: "Employee Benefits Bureau Open Enrollment Presentation",
+          url: "/documents/2023_2024_Open_Enrollment_Materials/1_EBB_2024_Open_Enrollment.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+      ],
+      generalInfo: [
+        {
+          linkName: "Administrative Guide",
+          url: "administrativeLetters.html",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Benefits Comparison Guide 2024",
+          url: "documents/Benefit_Comparison_Grid_FY24_Final_10-5-23.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Premium Rate Information",
+          url: "PremiumRates.html",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Benefit Plan Contacts",
+          url: "carrierContacts.html",
+          iconClass: "pdf",
+        },
+        {
+          linkName: "COBRA Resources",
+          url: "cobra.html",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Employee Self Service Instruction Guide",
+          url: "/documents/SHARE_Introduction_to_Employee_Self-Service12.24.19.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Forms, Guidelines and Policies Page",
+          url: "fgp.html",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Creditable Coverage Notice",
+          url: "FGPOpenFPN.html",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Benefits at a Glance Brochure (Trifold)",
+          url: "documents/Emply_Trifold_6_28_7_18.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "2021 Plan Year Archive",
+          url: "presentations2021.html",
+          iconClass: "bx-link",
+        },
+      ],
+      presentationsRec: [
+        {
+          linkName:
+            "New Hire and Qualifying Event Orientation Printable (English)",
+          url: "/documents/New-Employee-QE-Orientation-Final-Effective-7-1-22.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName:
+            "New Hire and Qualifying Event Orientation Printable (espanol)",
+          url: "/documents/New-Employee-QE-Orientation-Final-Effective-7-1-22-Spanish-Version.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName:
+            "New Hire and Qualifying Event Orientation Recording (English)",
+          url: "movies/REspinoza_Orientation_Recording_2022-06-24-14-07-21_Trim.mp4",
+          iconClass: "bx-play-circle",
+        },
+        {
+          linkName:
+            "New Hire and Qualifying Event Orientation Recording (espanol)",
+          url: "movies/YLopez_Orientation_Recording_2022-06-29_Trim.mp4",
+          iconClass: "bx-play-circle",
+        },
+      ],
+    },
+  },
+  //Presbyterian
+  {
+    carrierName: "Presbyterian Health Services",
+    imgURL: "/images/Presbyterian.png",
+    webAddress:
+      "https://www.phs.org/health-plans/employer-plans/state-of-new-mexico",
+    modalID: "presbyterianHealthServices",
+    content: {
+      openEnrollmentPY24: [
+        {
+          linkName: "Presbyterian Summary of Benefits and Coverage 2024",
+          url: "https://onbaseext.phs.org/PEL/DisplayDocument?ContentID=OB_000000022195",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "This is the Second OE Link",
+          url: "documents/2023_2024_Open_Enrollment_Materials/SONM_2024_Open_Enrollment_Presentation_Final_SWORD_Aetna.pptx",
+          iconClass: "bx-slideshow",
+        },
+        {
+          linkName: "Presbyterian and Aetna Network",
+          url: "documents/2023_2024_Open_Enrollment_Materials/Aetna_Network_Flyer_proof_2.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Presbyterian Overview of Benefits 2024",
+          url: "documents/2023_2024_Open_Enrollment_Materials/SONM_Brochure_2024_WEB_100223.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Presbyterian Health Services Presentation Video",
+          url: "movies/2023_2024_OE_Recorded_Presentations/Presbyterian_SONM_Plan_Year_2024.mp4",
+          iconClass: "bx-play-circle",
+        },
+      ],
+      generalInfo: [
+        {
+          linkName: "Information About PresRN",
+          url: "/documents/PresRN.PDF",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "MultiPlan Portal",
+          url: "https://www.multiplan.com/presbyterian",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Community Health Worker Program",
+          url: "https://www.mybenefitsnm.com/Documents/presentations2022/CHW_Brochure.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Presbyterian Tier 1 Directory",
+          url: "https://www2.phs.org/providers?insurance_plans=state-of-new-mexico-preferred-tier-1-network",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Presbyterian Tier 2 Directory",
+          url: "https://www2.phs.org/providers?insurance_plans=state-of-new-mexico-tier-2-network",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Presbyterian Fitness Pass",
+          url: "Documents/PHS_IBAC_Fitness_Pass_Flyer_PQ.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Adult Disabled Dependent Care Form",
+          url: "Documents/Presbyterian_Impaired_dependent_form.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Transition of Care Form",
+          url: "/documents/Transition%20of%20Care%20Form.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Presbyterian HIPAA Authorization Form",
+          url: "/documents/HIPAA_Auth_Form.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Communicating Feelings & Needs with Your Employer",
+          url: "/documents/CommFeelwEmplyr_PHP.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Staying Healthy in this New Environment",
+          url: "/documents/Staying_Health_in_this_New_Env-PHP.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Lifting Your Mood and Energy Level",
+          url: "/documents/AcknMoodandEnergy-PHP.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Transparency in Coverage",
+          url: "https://www.phs.org/tools-resources/member/your-care-your-choice/Pages/default.aspx",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "CVS Caremark Benefits Overview",
+          url: "/documents/Presbyterian_Preferred_Overview.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Talkspace para la salud conductual",
+          url: "/documents/TALKSPACE-FLYER-SPANISH.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        // {
+        //     linkName:'',
+        //     url:'',
+        //     iconClass:''
+        // },
+      ],
+      presentationsRec: [
+        {
+          linkName: "Presbyterian - Video Presentation",
+          url: "/documents/CY23_OE_Recordings_and_Presentations/5_PHS_CY23.mp4",
+          iconClass: "bx-play-circle",
+        },
+        {
+          linkName: "Presbyterian - Printable Presentation",
+          url: "documents/CY23_OE_Recordings_and_Presentations/Presbyterian.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+      ],
+    },
+  },
+  //BCBSNM
+  {
+    carrierName: "Blue Cross Blue Shield NM",
+    imgURL:
+      "https://www.bcbsnm.com/content/dam/bcbs/retail/images/logo/large-header-logo_nm.png",
+    webAddress: "",
+    modalID: "bcbsNM",
+    content: {
+      openEnrollmentPY24: [
+        // {
+        // linkName:'',
+        // url:'',
+        // iconClass:''
+        // },
+        {
+          linkName: "Blue Cross Blue Shield NM Presentation PDF",
+          url: "documents/2023_2024_Open_Enrollment_Materials/5_BCBS_2024_SONM_OE_Presentation_08_31_23_ Final.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Blue Cross Blue Shield NM Presentation Video",
+          url: "movies/2023_2024_OE_Recorded_Presentations/BCBSNM_SONM_Plan_Year_2024.mp4",
+          iconClass: "bx-play-circle",
+        },
+      ],
+      openEnrollmentPY23: [
+        {
+          linkName: "2023 SBC BCBSNM Blue Preferred Plus PPO Plan",
+          url: "/documents/SBC_266000_268390_0008_State of NM_PPO_01-01-2023 to 06-30-2023_2022-01-01_v1 (1).pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "2023 SBC BCBSNM HMO Plan",
+          url: "/documents/SBC_266000_266002_0007_State of NM_HMO_01-01-2023 to 06-30-2023_2015-01-01_v1.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS PPO Summary Plan Description 2023",
+          url: "/documents/Benefit Booklet PPO Final 07-01-2022.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS HMO Summary Plan Description 2023",
+          url: "/documents/Benefit Booklet HMO Final 07-01-2022.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS HMO Plan Highlights",
+          url: "/documents/2022_Open_Enrollment/BCBSNM/Blue_Cross_Blue_Shield_Benefit_Plan_Highlights_HMO.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS PPO Plan Highlights",
+          url: "/documents/2022_Open_Enrollment/BCBSNM/Blue_Cross_Blue_Shield_Benefit_Plan_Highlights_PPO.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+      ],
+      generalInfo: [
+        {
+          linkName: "Catapult Health - VirtualCheckup Saves Money and Lives",
+          url: "https://www.catapulthealth.com/",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "COVID-19 And Mental Health Wellness",
+          url: "documents/Livongo_COVID_mental-wellness-flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS HMO Member Guide 2022",
+          url: "documents/NM_Group_SONM_HMO_2022_Open_Enrollment_Member_Guide_Final_WEB.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS PPO Member Guide 2022",
+          url: "documents/NM_Group_SONM_PPO_2022_Open_Enrollment_Member_Guide_Final_WEB.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "BCBS Away From Home Care Program Information",
+          url: "https://www.bcbsnm.com/pdf/formfinder/afhc_guest_app_nm.pdf",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "What is the difference between an HMO and PPO Plan?",
+          url: "/documents/HMO%20vs%20PPO-2016.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Adult Disabled Dependent Care Form",
+          url: "https://www.bcbsnm.com/pdf/forms/disabled_dependent_cert_nm.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Transition of Care Form",
+          url: "https://www.mybenefitsnm.com/Documents/Transition%20of%20care%20-%20PPO%20nm_toc_request_ppo_060315.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Blue Access for Members",
+          url: "/documents/2022_Open_Enrollment/BCBSNM/Blue_Cross_Blue_Shield_Blue_Access_for_Members.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "About Blue Preferred Plus",
+          url: "https://www.mybenefitsnm.com/Documents/presentations2022/NM_Group_Local_Tiered_Network_Blue_Preferred_Plus_Provider_Finder_Member_Flier(3).pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Well onTarget",
+          url: "/documents/2022_Open_Enrollment/BCBSNM/Blue_Cross_Blue_Shield_Well_onTarget.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Wellbeing Management 24-7 Nurseline",
+          url: "/Documents/presentations2022/NM_Group_Wellbeing_Management_24-7_Nurseline_Member_Flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName:
+            "Digital Mental Health - Get a Mental Health Tune-Up... Online!",
+          url: "/Documents/NM-Group-Behavioral-Health-Digital-Mental-Health-Member-Flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName:
+            "Salud mental digital - Participe en un programa de salud mental... en l&iacute;nea!",
+          url: "/Documents/NM-SP-Group-Behavioral-Health-Digital-Mental-Health-Member-Flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "More Resources for Your Mental Wellbeing",
+          url: "/documents/NM-Group-Behavioral-Health-2023-All-Resources-Member-Flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "M&aacute;s recursos para su bienestar mental",
+          url: "/documents/NM-SP-Group-Behavioral-Health-2022-All-Resources-Member-Flier.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Tier 1 and Tier 2 Directory",
+          url: "https://www.bcbsnm.com/sonm/doctors-and-hospitals",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Transparency in Coverage",
+          url: "https://www.bcbsnm.com/asomrf?EIN=856000565",
+          iconClass: "bx-link",
+        },
+        {
+          linkName: "Find a Provider",
+          url: "documents/2022_Open_Enrollment/BCBSNM/Blue_Cross_Blue_Shield_Provider_Finder_Your_Doctor_Is_In.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "CVS Caremark Benefits Overview",
+          url: "/documents/BCBS_HMO_and_BCBS_Preferred_PPO_Overview.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+      ],
+      presentationsRec: [
+        {
+          linkName: "BCBS - Video Presentation",
+          url: "/documents/CY23_OE_Recordings_and_Presentations/3_BCBSNM_CY23.mp4",
+          iconClass: "bx-play-circle",
+        },
+        {
+          linkName: "BCBS - Printable Presentation",
+          url: "/documents/CY23_OE_Recordings_and_Presentations/BCBS.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+      ],
+    },
+  },
+  //Cigna
+  {
+    carrierName: "Cigna Health",
+    imgURL: "/images/New_Cigna Logo_2023.PNG",
+    webAddress: "https://connections.cigna.com/newmexico/",
+    modalID: "cignaHealth",
+    content: {
+      openEnrollmentPY24: [
+        {
+          linkName: "Cigna Health Presentation PDF",
+          url: "documents/2023_2024_Open_Enrollment_Materials/6_Cigna_State_of_New_Mexico_2024.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "Cigna Health Presentation Video",
+          url: "movies/2023_2024_OE_Recorded_Presentations/Cigna_Health_SONM_Plan_Year_2024.mp4",
+          iconClass: "bx-play-circle",
+        },
+        {
+          linkName: "Cigna Benefits Summary OAP 2024",
+          url: "documents/Bensum_OAP_750_01012024_StateofNewMexico_3343553.pdf",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "",
+          url: "",
+          iconClass: "bxs-file-pdf",
+        },
+        {
+          linkName: "",
+          url: "",
+          iconClass: "",
+        },
+      ],
+    },
+  },
+  // CVS Caremark
+  {
+    carrierName: "CVS Caremark Pharmaceutical",
+    imgURL: "/images/CVSCaremarkLogoRed.png",
+    webAddress: "https://www.caremark.com/",
+    modalID: "cvsHealth",
+    content: {},
+  },
+  // EAP
+  {
+    carrierName: "Employee Assistance Program",
+    imgURL: "/images/wellbeingsolution.jpg",
+    webAddress: "employeeAssistanceProgram.html",
+    modalID: "employeeAssistanceProgram",
+    content: {},
+  },
+  //Delta Dental
+  {
+    carrierName: "Delta Dental",
+    imgURL: "/images/deltadendal.png",
+    webAddress: "https://www.deltadentalnm.com/",
+    modalID: "deltaDental",
+    content: {},
+  },
+  // EyeMed
+  {
+    carrierName: "EyeMed Vision",
+    imgURL: "/images/EYEMED_logo-green-grey.jpg",
+    webAddress: "https://member.eyemedvisioncare.com/sonm/en",
+    modalID: "eyeMed",
+    content: {},
+  },
+  // The Hartford
+  {
+    carrierName: "The Hartford",
+    imgURL: "/images/hartfordlogo.png",
+    webAddress:
+      "https://gateway.on24.com/wcc/experience/eliteHartfordFireInsuranceCom/2594735/3725682/State%2520of%2520New%2520Mexico%2520Risk%2520Management%2520Division",
+    modalID: "theHartford",
+    content: {},
+  },
+  // FSA
+  {
+    carrierName: "Flexible Spending Account (FSA)",
+    imgURL: "/images/tree.png",
+    webAddress: "fsa.html",
+    modalID: "fsaOEPresentations",
+    content: {},
+  },
+  // Virta
+  {
+    carrierName: "Virta Health (PHP and BCBS)",
+    imgURL: "/images/virta_pres_bcbs_logo.png",
+    webAddress: "https://www.virtahealth.com/join/sonm",
+    modalID: "virtaModal",
+    content: {},
+  },
+  // Omada
+  {
+    carrierName: "Omada Health (Cigna)",
+    imgURL: "/images/omada_cigna_logo.jpg",
+    webAddress: "https://go.omadahealth.com/sonm",
+    modalID: "omadaModal",
+    content: {},
+  },
+  // Voluntary Benefits
+  {
+    carrierName: "Voluntary Benefits",
+    imgURL: "/images/2019open.jpg",
+    webAddress: "volben.html",
+    modalID: "voluntaryBenefits",
+    content: {},
+  },
+  //Wondr Health Program (BCBS)
+  {
+    carrierName: "Wondr Health Program (BCBS)",
+    imgURL: "images/Wondr_Logo_cropped.png",
+    webAddress: "https://enroll.wondrhealth.com/start?s=SONM",
+    modalID: "wondrProgram",
+    content: {},
+  },
+  //Hinge Health Program (BCBS)
+  {
+    carrierName: "Hinge Health Program (BCBS)",
+    imgURL: "images/Hinge_Health_logo.png",
+    webAddress: "https://hinge.health/stateofnewmexico",
+    modalID: "hingeHealth",
+    content: {},
+  },
+  //Disability Benefits
+  {
+    carrierName: "Disability Benefits",
+    imgURL: "images/easi.png",
+    webAddress: "disability.html",
+    modalID: "disabilityBenefits",
+    content: {},
+  },
+  //Catapult Health (BCBS)
+  {
+    carrierName: "Catapult Health (BCBS)",
+    imgURL: "images/catapult.jpg",
+    webAddress: "https://www.catapulthealth.com/",
+    modalID: "catapult",
+    content: {},
+  },
+  //Sword Pain Management (PHP)
+  {
+    carrierName: "Sword Pain Management (PHP)",
+    imgURL: "images/01_SwordLogo_digital_preferred_red_blue.png",
+    webAddress:
+      "https://meet.swordhealth.com/sonm?utm_source=n_a_sword&utm_medium=print&utm_campaign=offline_n_a&utm_content=posterflyer",
+    modalID: "swordHealth",
+    content: {},
+  },
+];
+
+export default carriers;
