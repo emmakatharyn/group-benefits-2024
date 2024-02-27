1b0e145228361fb87d3557c462e017b48948d930
Emma Reed <ereed@easitpa.com>
2024-02-27 11:29:28 -0700

    adjust grid item height, icon margin

---
+++ src/pages/QualifyingEventLPB.js
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+
+const handleClick = () => {
+  console.log("Modal tile was clicked");
+};
+
+function QualifyingEventLPB() {
+  return (
+    <div className='container portal-grid my-5'>
+      {/* modal trigger */}
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bxs-info-circle'></i>
+        What is a Qualifying Event?
+      </button>
+
+      <Link
+        to='https://eform.gsd.state.nm.us/ebb_benefitsenrollment.aspx'
+        className='btn btn-primary quicklink'
+        target='_blank'
+      >
+        <i className='bx bxs-file-pdf'></i>
+        Online Enrollment Form for LPB Qualifying Events
+      </Link>
+
+      {/* modal trigger */}
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bxs-info-circle'></i>
+        Printable Enrollment Forms for LPB Employees
+      </button>
+
+      <Link
+        to='/documents/EnrollmentChange_Legislators_FY21.pdf'
+        className='btn btn-primary quicklink'
+        target='_blank'
+      >
+        <i className='bx bxs-file-pdf'></i>
+        Legislator Enrollment Form for Qualifying Events
+      </Link>
+
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bx-shield-alt-2'></i> Supplemental Life Changes
+      </button>
+
+      <Link to='/lpb-employees' className='btn btn-primary quicklink'>
+        <i className='bx bxs-info-circle'></i>I'm a New Hire and Do NOT have a
+        Qualifying Event
+      </Link>
+    </div>
+  );
+}
+
+export default QualifyingEventLPB;
---
+++ src/pages/QualifyingEventState.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+const handleClick = () => {
+  console.log("Modal tile was clicked");
+};
+
+function QualifyingEventState() {
+  return (
+    <div className='container portal-grid my-5'>
+      {/* modal trigger */}
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bxs-info-circle'></i>
+        What is a Qualifying Event?
+      </button>
+
+      <Link
+        to='https://eform.gsd.state.nm.us/ebb_benefitsenrollment.aspx'
+        className='btn btn-primary quicklink'
+        target='_blank'
+      >
+        <i className='bx bxs-file-pdf'></i>
+        Online Enrollment Form for Qualifying Events
+      </Link>
+
+      <Link
+        to='/documents/FSA_Change_Form_Fill_1_1_23.pdf'
+        className='btn btn-primary quicklink'
+        target='_blank'
+      >
+        <i className='bx bx-link'></i>
+        FSA and Dependent Care Qualifying Event Form
+      </Link>
+
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bxs-car'></i>
+        Transit and Parking Benefit Changes
+      </button>
+
+      <button className='btn btn-primary quicklink' onClick={handleClick}>
+        <i class='bx bx-shield-alt-2'></i> Supplemental Life Changes
+      </button>
+
+      <Link to='/state-agency-employees' className='btn btn-primary quicklink'>
+        <i className='bx bxs-info-circle'></i>I'm a New Hire and Do NOT have a
+        Qualifying Event
+      </Link>
+    </div>
+  );
+}
+
+export default QualifyingEventState;
