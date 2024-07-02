8284477b8af751dfd43898d9ffaf39677032eb56
Emma Reed <ereed@easitpa.com>
2024-07-01 21:21:46 -0600

    new homepage layout

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -9,47 +9,51 @@ const items = [
     image: require("../Media/banner10.png"),
     link: "/open-enrollment",
     // description:
+    // this causes problems hmmmmmmmmmmmmmmmmmmm this is where i left off
+    //sonofabitch
     //   "EBB wants your feedback on some stuff and this said somethin else too but i forgot... it doesnt matter, i just wanna see what this looks like ahahah",
   },
   {
     title: "LPB Premium Rates for January 1, 2024",
     image: require("../Media/balloon-fiesta.jpg"),
-    link: "https://google.com/",
+    link: "/lpb-employees/premium-rates",
   },
   {
     title: "Newsletters and Updates",
     image: require("../Media/tim-mossholder-H6eaxcGNQbU-unsplash.jpg"),
-    link: "modal",
+    link: "/news",
   },
   {
     title: "No Cost VirtualCheckup by Catapult Health",
     image: require("../Media/rainbow.jpg"),
-    link: "documents/State of New Mexico - VC (Tasso) - Registration Poster.pdf",
+    link: "/documents/State of New Mexico - VC (Tasso) - Registration Poster.pdf",
   },
   {
-    title: "Employee 1095-C Important Information",
-    image: require("../Media/national-cancer-institute-L8tWZT4CcVQ-unsplash.jpg"),
-    link: "modal",
+    title: "Health Care Authority Notification to Payors",
+    image: require("../Media/rainbow.jpg"),
+    link: "/documents/GSD_EBB_HCA_SHB_Notification_to_Payors.pdf",
   },
   {
     title: "Virta and Omada Virtual Care",
     image: require("../Media/national-cancer-institute-L8tWZT4CcVQ-unsplash.jpg"),
-    link: "modal",
+    link: "/virta-and-omada",
   },
   {
     title: "COVID Vaccination Helpful Resources",
     image: require("../Media/sam-moghadam-khamseh-2rrsfMN4hn8-unsplash.jpg"),
-    link: "modal",
+    link: "/covid",
   },
   {
     title: "Medical Providers Tier 1 and Tier 2 Networks",
     image: require("../Media/bret-kavanaugh-MIfUp3FA5ek-unsplash.jpg"),
-    link: "https://nmpsia.com/",
+    // this one has TWO links. hmm. maybe link it to a single "page" that has these two links and a blurb or something
+    link: "https://www.bcbsnm.com/sonm/doctors-and-hospitals",
+    // link: "https://www.phs.org/health-plans/employer-plans/state-of-new-mexico"
   },
   {
-    title: "SONM Heart Health Challenge",
+    title: "Presbyterian Fitness Pass Membership",
     image: require("../Media/luke-porter-NEqEC7qa9FM-unsplash.png"),
-    link: "/documents/SoNM_Healthy_Heart_WT_2024.pdf",
+    link: "/documents/PHS_IBAC_Fitness_Pass_Flyer_PQ_2024.pdf",
   },
 ];
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,12 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <Carousel />
-      {/* update home address direct link */}
-      <div className='slide-in-link'>
-        <Link to='/share'>Click Here to Update Your Home Address!</Link>
-      </div>
-      {/* quick links */}
-      <div className='quicklinks-container'>
+      <div className="outer-flexbox">
+{/* quick links */}
+<div className='quicklinks-container'>
         <Link
           to='/employees'
-          className='btn btn-primary quicklink'
+          className='btn-primary quicklink'
           style={{
             backgroundImage: `linear-gradient(45deg, #1d1e53af, #2d46b486), url('images/white-sands-5075995_960_720.jpg')`,
           }}
@@ -25,7 +21,7 @@ function Home() {
         </Link>
         <Link
           to='/employers'
-          className='btn btn-primary quicklink'
+          className='btn-primary quicklink'
           style={{
             backgroundImage: `linear-gradient(45deg, #1d1e53af, #2d46b486),
             url('images/new-mexico-2499015_960_720.jpg')`,
@@ -35,7 +31,7 @@ function Home() {
         </Link>
         <Link
           to='/benefits-info'
-          className='btn btn-primary quicklink'
+          className='btn-primary quicklink'
           style={{
             backgroundImage: `linear-gradient(45deg, #1d1e53af, #2d46b486),
             url('images/pic3.jpg')`,
@@ -46,7 +42,7 @@ function Home() {
         <Link
           to='documents/Emply_Trifold_6_28_7_18.pdf'
           target='_blank'
-          className='btn btn-primary quicklink'
+          className='btn-primary quicklink'
           style={{
             backgroundImage: `linear-gradient(45deg, #1d1e53af, #2d46b486),
             url('images/pic4.jpg')`,
@@ -55,6 +51,10 @@ function Home() {
           Benefits at a Glance
         </Link>
       </div>
+      <Carousel />
+      </div>
+     
+      
       {/* about us */}
       <div id='aboutUs' className='mt-5'>
         <img
@@ -72,17 +72,10 @@ function Home() {
             <h3 className='display-5 my-3'>About Us</h3>
           </div>
           <p className='my-4'>
-            Welcome to the State of New Mexico's Employee Benefits Website. We
-            offer competitive, comprehensive medical, dental, vision,
-            pharmaceutical, Employee Assistance Program, basic and supplemental
-            life insurance, flexible spending accounts, and disability benefits
-            to enhance the lives of our employees and their qualified family
-            members.
+          Welcome to the State of New Mexico's Employee Benefits Website. We offer competitive, comprehensive medical, dental, vision, pharmaceutical, Employee Assistance Program, basic and supplemental life insurance, flexible spending accounts, and disability benefits to enhance the lives of our employees and their qualified family members.
           </p>
           <p className='lead'>
-            Employee Benefits Bureau &mdash; Providing the employees of the
-            State of New Mexico and participating Local Public Bodies the tools
-            to live a happier, healthier life.
+          Health Care Authority — Providing the employees of the State of New Mexico and participating Local Public Bodies the tools to live a happier, healthier life
           </p>
         </div>
       </div>
